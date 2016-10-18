ed6e2c13edaf618885bc847c0ecac5fab4580910
Jourdan Rodrigues <thiagojourdan@gmail.com>
2016-10-18 19:58:39 -0300

    Files compiler.

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,41 @@
-require('dotenv').config();
+require("dotenv").config();
 
 var gulp = require("gulp"),
     connect = require("gulp-connect"),
     useref = require("gulp-useref"),
     uglify = require("gulp-uglify"),
+    rename = require("gulp-rename"),
+    replace = require("gulp-replace"),
     cleanCss = require("gulp-clean-css"),
     gulpIf = require("gulp-if");
 
 var debug = process.env.DEBUG == "1";
 
-gulp.task("build", function() {
-    if (debug) console.log("Build skipped...");
-    else {
-        gulp
-            .src("templates/*.html")
-            .pipe(gulp.dest("dist/templates"));
-        gulp
-            .src("images/*.*")
-            .pipe(gulp.dest("dist/images"));
-        gulp
-            .src("index.html")
-            .pipe(useref())
-            .pipe(gulpIf('*.js', uglify()))
-            .pipe(gulpIf('*.css', cleanCss()))
-            .pipe(gulp.dest("dist"));
-    }
+gulp.task("compile", function() {
+    return gulp
+        .src("js/**/*.src.js")
+        .pipe(rename(function(path) {
+            path.dirname += "/js";
+            path.basename = path.basename.replace(".src", "");
+        }))
+        .pipe(replace("***apiHost***", process.env.API_HOST))
+        .pipe(gulp.dest(""));
+});
+
+// Last task before connection
+gulp.task("build", ["compile"], function() {
+    gulp
+        .src("templates/*.html")
+        .pipe(gulp.dest("dist/templates"));
+    gulp
+        .src("images/*.*")
+        .pipe(gulp.dest("dist/images"));
+    gulp
+        .src("index.html")
+        .pipe(useref())
+        .pipe(gulpIf('*.js', uglify()))
+        .pipe(gulpIf('*.css', cleanCss()))
+        .pipe(gulp.dest("dist"));
 });
 
 gulp.task("connect", function() {
@@ -35,10 +46,16 @@ gulp.task("connect", function() {
     });
 });
 
+var fileHandlerTask = (debug ? "compile" : "build");
+
 gulp.task("watch", function() {
-    gulp.watch(["css/*.css", "js/*.js", "index.html", "templates/*.html"], ["build"], function() {
-        connect.reload();
-    });
+    gulp.watch(
+        ["css/*.css", "js/*.js", "!js/index.js", "index.html", "templates/*.html", ".env"],
+        [fileHandlerTask],
+        function() {
+            connect.reload();
+        }
+    );
 });
 
-gulp.task("default", ["build", "connect", "watch"]);
+gulp.task("default", [fileHandlerTask, "connect", "watch"]);
