222bf2f08129b8bf34b0fca927c1f6bfdb801e45
Jourdan Rodrigues <thiagojourdan@gmail.com>
2016-11-12 03:05:54 -0300

    Added "plus" button to client list.

--- app/app.components.js
+++ app/app.components.js
@@ -1,4 +1,7 @@
 angular.module("controk")
+    .component("buttonPlus", {
+        templateUrl: "app/components/buttonPlus.html"
+    })
     .component("buttonV", {
         templateUrl: "app/components/buttonV.html"
     });
