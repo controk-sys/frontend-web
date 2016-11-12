a526f1bd2e08d6222f1626715636432f6c0e153b
Jourdan Rodrigues <thiagojourdan@gmail.com>
2016-11-12 11:27:16 -0300

    Added functionality to create employee.

--- tests/employees-spec.js
+++ tests/employees-spec.js
@@ -11,10 +11,8 @@ describe("The user", function() {
         })).toBe(0);
     });
     it("should be able to open the details of a employee", function() {
-        browser.get("");
+        browser.get("#/employees");
 
-        element(by.css(".navigation-link")).click();
-        element(by.css("a[ui-sref=employees]")).click();
         element.all(by.css("table tbody tr td.ng-binding")).first().click();
         expect(element.all(by.css("table")).count()).toBe(0); // Check if the table is gone
         expect(element.all(by.css(".ui-view [ng-controller]")).count()).toBe(1); // Check if the view was injected
@@ -23,5 +21,22 @@ describe("The user", function() {
         expect(element.all(by.css("label")).count()).toBe(15);
         expect(element.all(by.css("input")).count()).toBe(14);
         expect(element.all(by.css("select")).count()).toBe(1);
+    });
+    it("should be able to see the view for client creation", function() {
+        browser.get("#/employees");
+
+        expect(element.all(by.css("button-plus button")).count()).toBe(1);
+        element(by.css("button-plus button")).click();
+
+        expect(element.all(by.css("button-plus button")).count()).toBe(0);
+        expect(element.all(by.css("button-v button")).count()).toBe(1);
+
+        expect(element.all(by.css(".ui-view [ng-controller]")).count()).toBe(1); // Check if the view was injected
+        // 15 fields must exist
+        expect(element.all(by.css(".column")).count()).toBe(18); // + 3 field divisions
+        expect(element.all(by.css("label")).count()).toBe(15);
+        expect(element.all(by.css("input")).count()).toBe(14);
+        expect(element.all(by.css("select")).count()).toBe(1);
+        expect(element.all(by.css("select option")).count()).toBe(3);
     });
 });
