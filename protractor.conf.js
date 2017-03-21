7072ef00921dd5b390918c3980bec63bea1dd927
Jourdan Rodrigues <thiagojourdan@gmail.com>
2017-03-21 06:45:14 -0300

    :pencil: Protractor configuration improvement.

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,5 +1,5 @@
 exports.config = {
-  baseUrl: "http://localhost:" + (process.env.PORT || 8888) + "/",
+  baseUrl: `http://localhost:${process.env.PORT || 8888}/`,
   rootElement: ".controk", // http://stackoverflow.com/a/22518925
   directConnect: true,
   specs: ["tests/*-spec.js"]
