3b2923990f14efd1c770fdec308e7efe46161d9c
Jourdan Rodrigues <thiagojourdan@gmail.com>
2017-06-15 14:15:27 -0300

    :fire: Remove protractor conf.

--- protractor.conf.js
+++
@@ -1,6 +0,0 @@
-exports.config = {
-  baseUrl: `http://localhost:${process.env.PORT || 8888}/`,
-  rootElement: ".controk", // http://stackoverflow.com/a/22518925
-  directConnect: true,
-  specs: ["tests/*-spec.js"]
-}
