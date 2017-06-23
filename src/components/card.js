5be2185c2c4bcce595a79b39043350454474dbe3
Jourdan Rodrigues <thiagojourdan@gmail.com>
2017-06-23 18:20:23 -0300

    :lipstick: :sparkles: Allow lines in card body and improve shape.

--- src/components/card.js
+++ src/components/card.js
@@ -4,12 +4,16 @@ import './card.css'
 class Card extends Component {
   //noinspection JSMethodCanBeStatic
   render () {
+    let body = this.props.body, lines = body.split('\n')
+    if (lines.length > 1) {
+      body = lines.map((item, i) => <span key={i}>{item}<br/></span>)
+    }
     return (
       <div className="mdl-card mdl-shadow--2dp">
         <div className="mdl-card__title">
           <h2 className="mdl-card__title-text">{ this.props.title }</h2>
         </div>
-        <div className="mdl-card__supporting-text">{ this.props.body }</div>
+        <div className="mdl-card__supporting-text">{ body }</div>
         <div className="mdl-card__menu">
           <button className="mdl-button mdl-button--icon mdl-js-button mdl-js-ripple-effect">
             <i className="material-icons">edit</i>
