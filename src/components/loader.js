74a249a7b7b36165df37113024c587c57c50a35f
Jourdan Rodrigues <thiagojourdan@gmail.com>
2017-06-23 18:17:37 -0300

    :ambulance: Fix DOM node update on loader.

--- src/components/loader.js
+++ src/components/loader.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react' // eslint-disable-line no-unused-vars
+import {findDOMNode} from 'react-dom'
 
 class Loader extends Component {
   render () {
-    let [className, display] = this.props.isActive ? [' is-active', 'null'] : ['', 'none']
-    return <div className={'mdl-spinner mdl-js-spinner' + className} style={{display: display}} />
+    let style = {display: this.props.isActive ? 'inline-block' : 'none'}
+    return <div className='mdl-spinner mdl-js-spinner is-active' style={style}/>
+  }
+
+  componentDidMount () {
+    //noinspection JSCheckFunctionSignatures
+    componentHandler.upgradeElement(findDOMNode(this))
   }
 }
 
