f10f1f2c82471d9a59ab4ca1e0836a493c8a362d
Jourdan Rodrigues <thiagojourdan@gmail.com>
2017-06-22 23:22:22 -0300

    :sparkles: Add client and supplier list.

--- src/clients/list.js
+++ src/clients/list.js
@@ -1,14 +1,29 @@
 import React, { Component } from 'react' // eslint-disable-line no-unused-vars
+import axios from 'axios' // eslint-disable-line no-unused-vars
+import Card from '../components/card' // eslint-disable-line no-unused-vars
+import Loader from '../components/loader' // eslint-disable-line no-unused-vars
 import './list.css'
 
 class ClientList extends Component {
-  //noinspection JSMethodCanBeStatic
+  constructor (props) {
+    super(props)
+    this.state = {clients: [], isLoading: true}
+  }
+
   render () {
     return (
       <div className="clients-list">
-        <span>Client list.</span>
+        <Loader isActive={this.state.isLoading}/>
+        {this.state.clients.map((item, i) => <Card key={i} title={item.name} body={`${item.cpf} ${item.email}`}/>)}
       </div>
     )
+  }
+
+  componentDidMount () {
+    axios.get(process.env.API_URL + 'clients/')
+      .then((response) => {
+        this.setState({clients: response.data, isLoading: false})
+      })
   }
 }
 
--- src/suppliers/list.js
+++ src/suppliers/list.js
@@ -1,14 +1,31 @@
 import React, { Component } from 'react' // eslint-disable-line no-unused-vars
+import axios from 'axios' // eslint-disable-line no-unused-vars
+import Card from '../components/card' // eslint-disable-line no-unused-vars
+import Loader from '../components/loader' // eslint-disable-line no-unused-vars
 import './list.css'
 
 class SupplierList extends Component {
-  //noinspection JSMethodCanBeStatic
+  constructor (props) {
+    super(props)
+    this.state = {suppliers: [], isLoading: true}
+  }
+
   render () {
     return (
       <div className="suppliers-list">
-        <span>Supplier list.</span>
+        <Loader isActive={this.state.isLoading}/>
+        {this.state.suppliers.map(
+          (item, i) => <Card key={i} title={item.trading_name} body={`${item.cnpj} ${item.email}`}/>
+        )}
       </div>
     )
+  }
+
+  componentDidMount () {
+    axios.get(process.env.API_URL + 'suppliers/')
+      .then((response) => {
+        this.setState({suppliers: response.data, isLoading: false})
+      })
   }
 }
 
