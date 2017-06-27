b1c22a3121faa1e76206c6a254dae34764cf23ce
Jourdan Rodrigues <thiagojourdan@gmail.com>
2017-06-27 03:47:28 -0300

    :sparkles: :tractor: Create a navigation drawer component.

---
+++ src/components/navigationDrawer.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import { Route, Link } from 'react-router-dom' // eslint-disable-line no-unused-vars
+import PropTypes from 'prop-types'
+
+import 'material-design-lite'
+import '../../node_modules/material-design-lite/dist/material.min.css'
+
+class NavigationDrawer extends React.Component {
+  render () {
+    return (
+      <div className="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+        <header className="mdl-layout__header">
+          <div className="mdl-layout__header-row">
+            <span className="mdl-layout-title">Controk</span>
+          </div>
+        </header>
+        <div className="mdl-layout__drawer">
+          <nav className="mdl-navigation">
+            <Link className="mdl-navigation__link" to="/employees">Employees</Link>
+            <Link className="mdl-navigation__link" to="/suppliers">Suppliers</Link>
+            <Link className="mdl-navigation__link" to="/clients">Clients</Link>
+            <Link className="mdl-navigation__link" to="/shipments">Shipments</Link>
+            <Link className="mdl-navigation__link" to="/products">Products</Link>
+            <Link className="mdl-navigation__link" to="/stock">Stock</Link>
+          </nav>
+        </div>
+        <main className="mdl-layout__content">
+          <div className="page-content">
+            {this.props.children}
+          </div>
+        </main>
+      </div>
+    )
+  }
+
+  componentDidMount() {
+    /**
+     * Add click listeners to drawer items to close it
+     */
+    let closeDrawer = () => {
+      let obfuscator = document.querySelector('.mdl-layout__obfuscator')
+      let drawer = document.querySelector('.mdl-layout__drawer')
+
+      obfuscator.className = obfuscator.className.replace(/\s?is-visible/, '')
+      drawer.className = drawer.className.replace(/\s?is-visible/, '')
+    }
+
+    document.querySelectorAll('.mdl-navigation__link').forEach((item) => {
+      item.addEventListener('click', closeDrawer)
+    })
+  }
+}
+
+NavigationDrawer.propTypes = {
+  children: PropTypes.instanceOf(Route)
+}
+
+export default NavigationDrawer
--- src/index.js
+++ src/index.js
@@ -1,44 +1,22 @@
 import React from 'react' // eslint-disable-line no-unused-vars
 import { render } from 'react-dom'
-import { Route, HashRouter, Link } from 'react-router-dom' // eslint-disable-line no-unused-vars
+import { HashRouter, Route } from 'react-router-dom' // eslint-disable-line no-unused-vars
 
-import './handlers/docReady'
-import './handlers/mdl'
+import NavigationDrawer from './components/navigationDrawer' // eslint-disable-line no-unused-vars
 
 import ClientList from './clients/list'
 import SupplierList from './suppliers/list'
 import EmployeeList from './employees/list'
 
-import 'material-design-lite'
 import './index.css'
-import '../node_modules/material-design-lite/dist/material.min.css'
 
 render(
   <HashRouter>
-    <div className="mdl-layout mdl-js-layout mdl-layout--fixed-header">
-      <header className="mdl-layout__header">
-        <div className="mdl-layout__header-row">
-          <span className="mdl-layout-title">Controk</span>
-        </div>
-      </header>
-      <div className="mdl-layout__drawer">
-        <nav className="mdl-navigation">
-          <Link className="mdl-navigation__link" to="/employees">Employees</Link>
-          <Link className="mdl-navigation__link" to="/suppliers">Suppliers</Link>
-          <Link className="mdl-navigation__link" to="/clients">Clients</Link>
-          <Link className="mdl-navigation__link" to="/shipments">Shipments</Link>
-          <Link className="mdl-navigation__link" to="/products">Products</Link>
-          <Link className="mdl-navigation__link" to="/stock">Stock</Link>
-        </nav>
-      </div>
-      <main className="mdl-layout__content">
-        <div className="page-content">
-          <Route path="/clients" component={ClientList}/>
-          <Route path="/employees" component={EmployeeList}/>
-          <Route path="/suppliers" component={SupplierList}/>
-        </div>
-      </main>
-    </div>
+    <NavigationDrawer>
+      <Route path="/clients" component={ClientList}/>
+      <Route path="/employees" component={EmployeeList}/>
+      <Route path="/suppliers" component={SupplierList}/>
+    </NavigationDrawer>
   </HashRouter>,
   document.getElementById('root')
 )
